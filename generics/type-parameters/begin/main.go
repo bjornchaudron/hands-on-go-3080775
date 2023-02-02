4d26bd927df8ed2087d9c80b781e7735729a171d
Björn Chaudron <19856490+bjornchaudron@users.noreply.github.com>
2023-02-02 17:54:35 +0000

    Completed Chapter 6

--- generics/type-parameters/begin/main.go
+++ generics/type-parameters/begin/main.go
@@ -12,7 +12,11 @@ func sumFloats(a, b float64) float64 {
 }
 
 // create generic sum function with type parameter T constrained to int and float64 types
-//
+func sum[T ~int | ~float64](a, b T) T {
+	return a + b
+}
+
+type specialInt int
 
 func main() {
 	// non-generic sum int function
@@ -22,10 +26,17 @@ func main() {
 	fmt.Println(sumFloats(1.3, 2.2))
 
 	// call on generic sum function
-	//
+	fmt.Println(sum(1, 2))
+	fmt.Println(sum(1.3, 2.2))
 
 	// define a compatible custom type call on generic sum function with it
-	//
+	x := specialInt(3)
+	y := specialInt(5)
+	fmt.Println(sum(x, y))
 }
 
 // list is a singly-linked list that holds values of any type
+type list[T any] struct {
+	next *list[T]
+	val  T
+}
