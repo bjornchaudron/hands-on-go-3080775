147d066080336df98144c825a2258bde24b7d8ec
Björn Chaudron <19856490+bjornchaudron@users.noreply.github.com>
2023-02-27 18:45:14 +0000

    Completed Chapter 8

--- concurrency/channel-basics/begin/main.go
+++ concurrency/channel-basics/begin/main.go
@@ -3,24 +3,33 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // sum calculates and prints the sum of numbers
-func sum(nums []int) {
+func sum(nums []int, ch chan<- int) {
 	sum := 0
 	for _, v := range nums {
 		sum += v
 	}
-	fmt.Println("Result:", sum)
+	ch <- sum
 }
 
 func main() {
 	nums := []int{1, 2, 3, 4, 5}
 
-	// invoke the sum function as a goroutine
-	go sum(nums)
+	ch := make(chan int)
 
-	// force main thread to sleep
-	time.Sleep(100 * time.Millisecond)
+	// invoke the sum function as a goroutine
+	go sum(nums, ch)
+	result := <-ch
+	fmt.Println("Result:", result)
+
+	ch2 := make(chan string, 2)
+	ch2 <- "James"
+	ch2 <- "Toni"
+	// buffer overflow 3>2
+	// ch2 <- "Johnny"
+
+	fmt.Println(<-ch2)
+	fmt.Println(<-ch2)
 }
--- concurrency/channel-non-blocking/begin/main.go
+++ concurrency/channel-non-blocking/begin/main.go
@@ -1,8 +1,26 @@
 // concurrency/channel-non-blocking/begin/main.go
 package main
 
-func main() {
-	// declare a signal channel to read from
+import (
+	"fmt"
+	"time"
+)
 
-	// use a default case in select to prevent blocking
+func main() {
+	timeChan := time.After(200 * time.Millisecond)
+	timeChan2 := time.After(400 * time.Millisecond)
+
+	for {
+		select {
+		case <-timeChan:
+			fmt.Println("timeChan1")
+			return
+		case <-timeChan2:
+			fmt.Println("timeChan2")
+			return
+		default:
+			fmt.Println("default")
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
 }
